2473b9ee2dde9abd49c43b77184ed91ab3e34b7d
bdeme004 <43201288+bdeme004@users.noreply.github.com>
2018-10-11 15:05:33 -0400

    Add files via upload

---
+++ milestone1dump.sql
@@ -0,0 +1,225 @@
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: channels
+-- ------------------------------------------------------
+-- Server version	8.0.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '8c1f5630-cd85-11e8-9a15-54e1adad9a7e:1-14,
+a4c821dc-cd85-11e8-8d9b-54e1adad9a7e:1-34';
+
+--
+-- Table structure for table `channel1`
+--
+
+DROP TABLE IF EXISTS `channel1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `channel1` (
+  `user` varchar(10) NOT NULL,
+  `body` varchar(255) NOT NULL,
+  `creation` timestamp NOT NULL,
+  `chKey` double NOT NULL,
+  PRIMARY KEY (`chKey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `channel1`
+--
+
+LOCK TABLES `channel1` WRITE;
+/*!40000 ALTER TABLE `channel1` DISABLE KEYS */;
+/*!40000 ALTER TABLE `channel1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `channel2`
+--
+
+DROP TABLE IF EXISTS `channel2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `channel2` (
+  `user` varchar(10) NOT NULL,
+  `body` varchar(255) NOT NULL,
+  `creation` timestamp NOT NULL,
+  `chKey` double NOT NULL,
+  PRIMARY KEY (`chKey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `channel2`
+--
+
+LOCK TABLES `channel2` WRITE;
+/*!40000 ALTER TABLE `channel2` DISABLE KEYS */;
+/*!40000 ALTER TABLE `channel2` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `channel3`
+--
+
+DROP TABLE IF EXISTS `channel3`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `channel3` (
+  `user` varchar(10) NOT NULL,
+  `body` varchar(255) NOT NULL,
+  `creation` timestamp NOT NULL,
+  `chKey` double NOT NULL,
+  PRIMARY KEY (`chKey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `channel3`
+--
+
+LOCK TABLES `channel3` WRITE;
+/*!40000 ALTER TABLE `channel3` DISABLE KEYS */;
+/*!40000 ALTER TABLE `channel3` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `channel4`
+--
+
+DROP TABLE IF EXISTS `channel4`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `channel4` (
+  `user` varchar(10) NOT NULL,
+  `body` varchar(255) NOT NULL,
+  `creation` timestamp NOT NULL,
+  `chKey` double NOT NULL,
+  PRIMARY KEY (`chKey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `channel4`
+--
+
+LOCK TABLES `channel4` WRITE;
+/*!40000 ALTER TABLE `channel4` DISABLE KEYS */;
+/*!40000 ALTER TABLE `channel4` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `channel5`
+--
+
+DROP TABLE IF EXISTS `channel5`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `channel5` (
+  `user` varchar(10) NOT NULL,
+  `body` varchar(255) NOT NULL,
+  `creation` timestamp NOT NULL,
+  `chKey` double NOT NULL,
+  PRIMARY KEY (`chKey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `channel5`
+--
+
+LOCK TABLES `channel5` WRITE;
+/*!40000 ALTER TABLE `channel5` DISABLE KEYS */;
+/*!40000 ALTER TABLE `channel5` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-11 15:03:07
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: users
+-- ------------------------------------------------------
+-- Server version	8.0.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '8c1f5630-cd85-11e8-9a15-54e1adad9a7e:1-14,
+a4c821dc-cd85-11e8-8d9b-54e1adad9a7e:1-34';
+
+--
+-- Table structure for table `usertable`
+--
+
+DROP TABLE IF EXISTS `usertable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `usertable` (
+  `handle` varchar(16) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`handle`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usertable`
+--
+
+LOCK TABLES `usertable` WRITE;
+/*!40000 ALTER TABLE `usertable` DISABLE KEYS */;
+INSERT INTO `usertable` VALUES ('@chick','Chick Hicks','@chick','chinga@cars.com'),('@doc','Doc Hudson','@doc','hornet@rsprings.gov'),('@mater','Tow Mater','@mater','mater@rsprings.gov'),('@mcmissile','Finn McMissile','@mcmissile','topsecret@agent.org'),('@mcqueen','Lightning McQueen','@mcqueen','kachow@rusteze.com'),('@sally','Sally Carrera','@sally','porsche@rsprings.gov');
+/*!40000 ALTER TABLE `usertable` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-11 15:03:08
