00c4c716671af0c75ea7c5fc942ae563d958d667
bdeme004 <43201288+bdeme004@users.noreply.github.com>
2018-10-16 11:57:13 -0400

    Update mint.php

--- mint.php
+++ mint.php
@@ -35,7 +35,7 @@ $conn= set_connection("channels");
 </form>
 <?php 
 
-$stmt="INSERT INTO channel3 (user, body, chKey) VALUES (?, ?, now(4))";
+$stmt="INSERT INTO channel3 (user, body) VALUES (?, ?)";
 
 $sql = $conn->prepare($stmt);
 
