8c6d832d2155d461b7923b416dbeb8a48cddda35
bdeme004 <43201288+bdeme004@users.noreply.github.com>
2018-10-30 14:38:40 -0400

    Add files via upload

--- milestone2dump.sql
+++ milestone2dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 30, 2018 at 06:25 PM
+-- Generation Time: Oct 30, 2018 at 06:37 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -39,7 +39,14 @@ CREATE TABLE IF NOT EXISTS `heliotrope` (
   `chReplies` int(11) NOT NULL DEFAULT '0' COMMENT 'This isn''t strictly necessary. But I sort of suspect it''s good to have.',
   `chUpdated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'This isn''t strictly necessary. But I sort of suspect it''s good to have.',
   PRIMARY KEY (`chIndex`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `heliotrope`
+--
+
+INSERT INTO `heliotrope` (`chIndex`, `chTitle`, `chPrivate`, `chAllowedUsers`, `chReplies`, `chUpdated`) VALUES
+('1', 'Very nervous!', 0, 'ALL', 0, '2018-10-30 10:17:27');
 
 -- --------------------------------------------------------
 
@@ -92,6 +99,13 @@ CREATE TABLE IF NOT EXISTS `mint` (
   PRIMARY KEY (`chIndex`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `mint`
+--
+
+INSERT INTO `mint` (`chIndex`, `chTitle`, `chPrivate`, `chAllowedUsers`, `chReplies`, `chUpdated`) VALUES
+('t15409236916362', 'Secret Agent Meeting', 0, 'ALL', 0, '2018-10-30 14:21:31');
+
 -- --------------------------------------------------------
 
 --
@@ -108,6 +122,14 @@ CREATE TABLE IF NOT EXISTS `tearose` (
   `chUpdated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'This isn''t strictly necessary. But I sort of suspect it''s good to have.',
   PRIMARY KEY (`chIndex`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tearose`
+--
+
+INSERT INTO `tearose` (`chIndex`, `chTitle`, `chPrivate`, `chAllowedUsers`, `chReplies`, `chUpdated`) VALUES
+('t15409220590683', 'aaaaa', 0, 'ALL', 0, '2018-10-30 13:54:19'),
+('t15409228019388', 'Counting Off', 0, 'ALL', 0, '2018-10-30 14:06:41');
 --
 -- Database: `threads`
 --
@@ -128,7 +150,14 @@ CREATE TABLE IF NOT EXISTS `t15409236916362` (
   `chKey` int(11) NOT NULL AUTO_INCREMENT,
   `avatar` text NOT NULL,
   PRIMARY KEY (`chKey`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=284 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t15409236916362`
+--
+
+INSERT INTO `t15409236916362` (`user`, `body`, `creation`, `chKey`, `avatar`) VALUES
+('bdemerch', 'SECRET AGENT MEETING', '2018-10-30 18:22:52', 1, '15408921637358.png');
 --
 -- Database: `users`
 --
@@ -152,6 +181,20 @@ CREATE TABLE IF NOT EXISTS `usertable` (
   PRIMARY KEY (`handle`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `usertable`
+--
+
+INSERT INTO `usertable` (`handle`, `name`, `passcode`, `email`, `avatar`, `comment`) VALUES
+('@mater', 'Tow Mater', '@mater', 'mater@rsprings.gov', '15408259583499.png', 'Tow trucks tow things.'),
+('@sally', 'Sally Carrera', '@sally', 'porsche@rsprings.gov', '15408259634168.png', 'Zoom Zoom.'),
+('@doc', 'Doc Hudson', '@doc', 'hornet@rsprings.gov', '15408259361210.png', 'Zoom Zoom.'),
+('@mcmissile', 'Finn McMissile', '@mcmissile', 'topsecret@agent.org', '15408259411731.png', 'Zoom Zoom?'),
+('@mcqueen', 'Lightning McQueen', '@mcqueen', 'kachow@rusteze.com', '15408259521527.png', 'ZOOM ZOOM!'),
+('@chick', 'Chick Hicks', '@chick', 'chinga@cars.com', '15408259306433.png', 'Zoom zoom.'),
+('bdemerch', 'Bethany DeMerchant', 'bdemerch', 'bdeme004@odu.edu', '15408921637358.png', 'I added my own username because I kept having to stop and look up the car names.'),
+('@TheMoosh', 'Misha', '@themoosh', 'moosh@moo.sh', 'default_img.png', 'STOP CODING AND PET ME');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
