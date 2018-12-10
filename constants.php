1ad053e873bd7bf98e2b062ac1ec17a342a5ad35
bdeme004 <43201288+bdeme004@users.noreply.github.com>
2018-12-10 08:17:04 -0500

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,16 @@
+<?php
+
+define("DEFAULT_IMG", "sysimg/default-img.png");
+define("DEFAULT_INF", "sysimg/default-img-inf.png");
+
+define("ADD_IMG_DEF", "sysimg/add-img.png");
+define("ADD_FILE_DEF", "sysimg/file-gen-30.png");
+define("ADD_FILE_OK", "sysimg/file-gen-95.png" );
+define("ADD_FILE_THUMB", "sysimg/file-gen.png");
+
+define("IMG_DIR", "upload/img/");
+define("FILE_DIR", "upload/files/");
+define("GRAV_EXT", "?d=identicon&s=50");
+
+define("GH_CLIENT_ID", "ccc7822e7e7d2aea391b");
+?>
